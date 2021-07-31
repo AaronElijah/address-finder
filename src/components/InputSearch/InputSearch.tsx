4ba1deb17713561ff6178e19ecce16e2a4e439f9
AaronElijah <aaronzakelijah@googlemail.com>
2021-07-31 18:20:54 +0100

    feat: add new input search component

---
+++ src/components/InputSearch/InputSearch.tsx
@@ -0,0 +1,9 @@
+import "./InputSearch.css";
+
+export const InputSearch = () => {
+  return (
+    <form className="form">
+      <input type="search" className="input"></input>
+    </form>
+  );
+};
