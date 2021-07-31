baee7df32531475323ef6ad4a10c22935b24e426
AaronElijah <aaronzakelijah@googlemail.com>
2021-07-31 17:22:39 +0100

    refactor: use select for dropdown

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,12 +1,34 @@
 import "./Dropdown.css";
 
+// const;
+
 export const Dropdown = () => {
+  const defaultMessage = "Select years";
+  const options = [
+    { label: "1 year", value: 1 },
+    { label: "2 years", value: 2 },
+  ];
   return (
-    <div className="dropdown">
-      <div style={{ width: "100%" }}>
-        <span className="label">{"Hello"}</span>
-        <span className="toggle-arrow"></span>
-      </div>
-    </div>
+    // <div>
+    //   <div className="dropdown-box">
+    //     <div className="contents">
+    //       <span className="label">{"Hello"}</span>
+    //       <span className="toggle-arrow"></span>
+    //     </div>
+    //   </div>
+    //   <div className="dropdown-list">
+    //     <ul>
+    //       <li>{"1 Year"}</li>
+    //     </ul>
+    //   </div>
+    // </div>
+    <select className="dropdown" required>
+      <option style={{ display: "none" }} selected>
+        {defaultMessage}
+      </option>
+      {options.map((option) => (
+        <option value={option.value}>{option.label}</option>
+      ))}
+    </select>
   );
 };
